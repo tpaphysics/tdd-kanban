cbdedee594a568b4193431e4ffa91d326b601ba5
tpaphysics <physics.thiago@gmail.com>
2022-10-09 20:16:33 -0300

    style: create app scene

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import Counter from "./Components/Example/Counter/Counter";
+import { Flex } from '@chakra-ui/react';
+import Counter from './Components/Example/Counter/Counter';
 
 function App() {
-  return <Counter />;
+  return (
+    <Flex
+      w='100vw'
+      h='100vh'
+      flexDir='column'
+      bgImage='url(scene.svg)'
+      backgroundRepeat='no-repeat'
+      bgPosition='bottom'
+      backgroundPosition='95% 80%'
+    ></Flex>
+  );
 }
 
 export default App;
