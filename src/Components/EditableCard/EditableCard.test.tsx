0f87651fe04c157d29d2d04a1998c7184381a73f
tpaphysics <physics.thiago@gmail.com>
2022-10-10 13:17:38 -0300

    test: should it added finished when click in tag

--- src/Components/EditableCard/EditableCard.test.tsx
+++ src/Components/EditableCard/EditableCard.test.tsx
@@ -10,4 +10,10 @@ describe('EditableCard.tsx test', () => {
     const { getByText } = render(<EditableCard />);
     expect(() => getByText(/finished/)).toThrow();
   });
+
+  it('Should it added finished when click in tag', () => {
+    const { getByTestId, getByText } = render(<EditableCard />);
+    fireEvent.click(getByTestId('tag-1'));
+    expect(getByText('Finished')).toBeInTheDocument();
+  });
 });
