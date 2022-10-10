e54897b19004ccfc6418e7fc22f2abea27c339ef
tpaphysics <physics.thiago@gmail.com>
2022-10-10 12:25:51 -0300

    style: addition of trash can icon and end of task tag

--- src/Components/EditableCard/index.tsx
+++ src/Components/EditableCard/index.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
-import { Box, Editable, EditableInput, EditablePreview, Flex, Input, Text } from '@chakra-ui/react';
+import { BsTrashFill } from 'react-icons/bs';
+import { GiRapidshareArrow } from 'react-icons/gi';
+import { BsCheckCircle } from 'react-icons/bs';
+
+import {
+  Box,
+  Editable,
+  EditableInput,
+  EditablePreview,
+  Flex,
+  Icon,
+  Input,
+  Tag,
+  TagLabel,
+  TagLeftIcon,
+  Text,
+} from '@chakra-ui/react';
 
 function EditableCard() {
-  const card = { id: 1, task: 'Task', tag: 'Tag' };
+  const card = { id: 1, task: 'Task', tag: 'sucess' };
   const colorTag = 'BLUE';
   return (
     <Box
@@ -16,37 +32,44 @@ function EditableCard() {
       w='275px'
       h='90px'
     >
-      <Editable
-        defaultValue={card.task}
-        fontWeight='500'
-        fontSize='16px'
-        lineHeight='21px'
-        variant='none'
-      >
-        <EditablePreview />
-        <Input
+      <Flex alignItems={'center'} justifyContent='space-between'>
+        <Editable
+          defaultValue={card.task}
           fontWeight='500'
           fontSize='16px'
           lineHeight='21px'
-          as={EditableInput}
-          borderRadius='5px'
-          focusBorderColor={colorTag}
-          px='2px'
-          h='28px'
-        />
-      </Editable>
+          variant='none'
+        >
+          <EditablePreview />
+          <Input
+            fontWeight='500'
+            fontSize='16px'
+            lineHeight='21px'
+            as={EditableInput}
+            borderRadius='5px'
+            focusBorderColor={colorTag}
+            px='2px'
+            h='28px'
+          />
+        </Editable>
 
-      <Flex
-        w='49px'
-        h='24px'
-        borderRadius='5px'
-        bg={colorTag}
-        alignItems='center'
-        justifyContent='center'
-      >
-        <Text fontWeight='700' fontSize='12px' color='WHITE'>
-          {card.tag}
+        <Icon as={BsTrashFill} color={colorTag} cursor={'pointer'} />
+      </Flex>
+      <Flex alignItems='center' justifyContent='space-between'>
+        <Text>
+          <Tag size={'sm'} variant='solid' bg={colorTag} fontStyle='italic'>
+            <TagLeftIcon as={GiRapidshareArrow} />
+            <TagLabel fontWeight='700' fontSize='12px' color='WHITE'>
+              {card.tag}
+            </TagLabel>
+          </Tag>
         </Text>
+        <Flex color={'green.400'} alignItems='center' fontStyle='italic'>
+          <Icon as={BsCheckCircle} w='10px' h='12px' />
+          <Text ml='2px' fontSize='10px'>
+            Finished
+          </Text>
+        </Flex>
       </Flex>
     </Box>
   );
