be6337260b23e467acec241f5a1785caded68b84
tpaphysics <physics.thiago@gmail.com>
2022-10-11 14:26:21 -0300

    test: should it updateded newTask value for TEST

--- src/Components/FormAddCard/hook/useFormAddCard.test.ts
+++ src/Components/FormAddCard/hook/useFormAddCard.test.ts
@@ -1,4 +1,5 @@
 import { act, renderHook } from '@testing-library/react';
+import React from 'react';
 import { describe, it } from 'vitest';
 import { useFormAddCard } from './useFormAddCard';
 
@@ -25,5 +26,15 @@ describe('useFormAddCard hook test', () => {
       result.current.newTask;
     });
     expect(result.current.newTask).toEqual('');
+  });
+  it('handleChange, should it updateded newTask value for TEST', () => {
+    const { result } = renderHook(useFormAddCard);
+    const mockedEvent = { target: { value: 'TEST' } } as React.ChangeEvent<HTMLInputElement>;
+
+    act(() => {
+      result.current.handleChange(mockedEvent);
+    });
+
+    expect(result.current.newTask).toEqual('TEST');
   });
 });
