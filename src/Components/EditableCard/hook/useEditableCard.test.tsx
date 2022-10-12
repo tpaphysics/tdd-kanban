da8496cb6d4572b62c373aa533e01ca6e26ae1f0
tpaphysics <physics.thiago@gmail.com>
2022-10-12 16:27:56 -0300

    refactor: refactoring of c after add useList hook in useEditableCard

--- src/Components/EditableCard/hook/useEditableCard.test.tsx
+++ src/Components/EditableCard/hook/useEditableCard.test.tsx
@@ -1,25 +1,41 @@
+import { BoxProps } from '@chakra-ui/react';
 import { act, renderHook } from '@testing-library/react';
-import { describe, it } from 'vitest';
+import { describe, it, vi } from 'vitest';
+import lists from '../../../data/lists';
+import * as useList from '../../../Hooks/useList';
+import KanbanListProvider from '../../../Hooks/useList/Provider';
 import { useEditableCard } from './useEditableCard';
 
 describe('useEditableCard hook test', () => {
+  useList;
+  const mockedList = lists[0];
+  const mockedCard = mockedList.cards[0];
+  const mockedUseList = vi
+    .spyOn(useList, 'useList')
+    .mockImplementation(
+      () => ({ list: mockedList, AddCard: vi.fn(), removedCard: vi.fn() } as any),
+    );
+  const wrapper = ({ children }: BoxProps) => (
+    <KanbanListProvider initialList={mockedList}>{children}</KanbanListProvider>
+  );
+
   it('should be false the finished state by default', () => {
-    const { result } = renderHook(useEditableCard);
+    const { result } = renderHook(() => useEditableCard(mockedCard), { wrapper });
     act(() => {
       result.current.finished;
     });
-    expect(result.current.finished).toEqual(false);
+    expect(result.current.finished).toEqual(true);
   });
   it('handleClickTag, should be updateded finished state for true', () => {
-    const { result } = renderHook(useEditableCard);
+    const { result } = renderHook(() => useEditableCard(mockedCard), { wrapper });
 
     act(() => {
       result.current.handleClickTag();
     });
-    expect(result.current.finished).toEqual(true);
+    expect(result.current.finished).toEqual(false);
   });
   it('handleOnBlur, should contain (...) when make blur and when mockerLargeTaskValue for bigger than 25', () => {
-    const { result } = renderHook(useEditableCard);
+    const { result } = renderHook(() => useEditableCard(mockedCard), { wrapper });
     const mockedLargeTaskValue = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
 
     act(() => {
@@ -33,7 +49,7 @@ describe('useEditableCard hook test', () => {
     expect(result.current.task.includes('...')).toEqual(true);
   });
   it('handleClickTag, should be updateded finished value for (Edit task) when input to be empty', () => {
-    const { result } = renderHook(useEditableCard);
+    const { result } = renderHook(() => useEditableCard(mockedCard), { wrapper });
 
     act(() => {
       result.current.setTask('');
