46b528847e79f800e9bcf3aa6d9f4515b5a50851
tpaphysics <physics.thiago@gmail.com>
2022-10-21 11:52:41 -0300

    test: should be moved card for other list in other column

--- src/Hooks/useKanban/Provider/hook/useKanbanProvider.test.tsx
+++ src/Hooks/useKanban/Provider/hook/useKanbanProvider.test.tsx
@@ -32,6 +32,24 @@ describe('useKanbanProvider hook test', () => {
     },
     combine: null,
   };
+  const mockedMovedCard2 = {
+    draggableId:
+      '{"columnId":"836465da-523a-49bf-8ff2-33a4a2df275b","cardId":"36c9f0eb-12f8-4d22-8b12-5aae52d1f339"}',
+    type: 'card',
+    source: {
+      index: 0,
+      droppableId:
+        '{"listId":"a4557776-4933-404d-983e-3d502d73b332","columnId":"836465da-523a-49bf-8ff2-33a4a2df275b"}',
+    },
+    reason: 'DROP',
+    mode: 'FLUID',
+    destination: {
+      droppableId:
+        '{"listId":"196700bc-b6ff-4e6b-ad1b-3be61d771db7","columnId":"a8fdb3bc-8399-4e27-91da-3ddbb6cd9eb5"}',
+      index: 0,
+    },
+    combine: null,
+  };
 
   it('should be the initial column value equal mockedColumns', () => {
     const wrapper = ({ children }: BoxProps) => <KanbanProvider>{children}</KanbanProvider>;
@@ -82,6 +100,18 @@ describe('useKanbanProvider hook test', () => {
     });
 
     const response = result.current.columns[0].lists[0].cards[0].task;
+
+    expect(response).toBe('Task 2');
+  });
+  it('onDragEnd, Should be moved card for other list in other column', () => {
+    const wrapper = ({ children }: BoxProps) => <KanbanProvider>{children}</KanbanProvider>;
+    const { result } = renderHook(() => useKanban(), { wrapper });
+
+    act(() => {
+      result.current?.onDragEnd(mockedMovedCard2 as any);
+    });
+
+    const response = result.current.columns[1].lists[0].cards[0].task;
 
     expect(response).toBe('Task 2');
   });
