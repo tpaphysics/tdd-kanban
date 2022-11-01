1135b0985b036ca9b3044a5bd92edf161d1049ac
tpaphysics <physics.thiago@gmail.com>
2022-11-01 11:55:55 -0300

    refactor: refactor EditableCard.test.tsx after DragEditableCard.tsx cration

--- src/Components/EditableCard/EditableCard.test.tsx
+++ src/Components/EditableCard/EditableCard.test.tsx
@@ -8,7 +8,6 @@ import KanbanListProvider from '../../Hooks/useList/Provider';
 import columns from '../../data/columns';
 import KanbanColumnProvider from '../../Hooks/useColumn/Provider';
 import KanbanProvider from '../../Hooks/useKanban/Provider';
-import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 
 describe('EditableCard.tsx test', () => {
   const mockedColumn = columns[0];
@@ -19,23 +18,14 @@ describe('EditableCard.tsx test', () => {
     <KanbanProvider>
       <KanbanColumnProvider initialColumn={mockedColumn}>
         <KanbanListProvider initialList={mockedList}>
-          <DragDropContext onDragEnd={console.log}>
-            <Droppable droppableId='some_id'>
-              {(provided) => (
-                <div {...provided.droppableProps} ref={provided.innerRef}>
-                  <EditableCard card={mockedCard} cardIndex={0} />
-                </div>
-              )}
-            </Droppable>
-          </DragDropContext>
+          <EditableCard card={mockedCard} />
         </KanbanListProvider>
       </KanbanColumnProvider>
     </KanbanProvider>
   );
 
   it('Should finished tag not to be in component', () => {
-    const { getByText, debug } = render(<ContainerTest />);
-    debug();
+    const { getByText } = render(<ContainerTest />);
     expect(getByText('Finished')).toBeInTheDocument();
   });
 
@@ -62,11 +52,10 @@ describe('EditableCard.tsx test', () => {
     expect(getByText('Edit task')).toBeInTheDocument();
   });
   it('Should not be edited the card when click on (x) button', () => {
-    const { getByTestId, getByText, debug } = render(<ContainerTest />);
+    const { getByTestId, getByText } = render(<ContainerTest />);
     fireEvent.click(getByTestId(`edit-icon-${mockedCard.id}`));
     fireEvent.change(getByTestId(`input-${mockedCard.id}`), { target: { value: 'Click' } });
     fireEvent.click(getByTestId(`close-edit-icon-${mockedCard.id}`));
-    debug();
     expect(() => getByText('Click')).toThrow();
   });
 });
